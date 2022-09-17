9f2d5cf9661d5841adf4eedeb5e80d2b5b450b75
catherine <vuthi.esther@gmail.com>
2022-09-17 23:40:30 +0300

    adds social media icons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Image from "next/image";
 import Link from 'next/link';
-import { AiOutlineClose } from "react-icons/ai";
-import { AiOutlineMenu } from "react-icons/ai";
+import {
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiOutlineMail,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 import { MdDarkMode } from "react-icons/md";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import {BsFillPersonCheckFill} from "react-icons/bs"
 
 const Navbar = () => {
   return (
@@ -44,8 +50,66 @@ const Navbar = () => {
             </Link>
           </ul>
           <div className="flex md:hidden gap-6">
-            <MdDarkMode size={25} />
-            <AiOutlineMenu size={25} />
+            <MdDarkMode size={20} />
+            <AiOutlineMenu size={20} />
+          </div>
+        </div>
+      </div>
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/50">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-8 ease-in duration-500">
+          <div>
+            <div className="flex w-full items-center justify-between border-b border-gray-300 pb-5">
+              <Image
+                src="/../public/assets/logo/catherine-removebg-preview.png"
+                width="100"
+                height="35"
+                alt="/"
+              />
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose size={20} />
+              </div>
+            </div>
+            <div className="py-6 flex flex-col">
+              <ul className="uppercase">
+                <Link href="/">
+                  <li className="py-4 text-sm">Home</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">About</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">Skills</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">Portfolio</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">Contact</li>
+                </Link>
+              </ul>
+              <div className="pt-40">
+                <p className="uppercase tracking-widest text-[#6fb632]">
+                  Let's Connect
+                </p>
+                <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaGithub size={20} />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaLinkedinIn size={20} />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <AiOutlineTwitter size={20} />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <AiOutlineMail size={20} />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <BsFillPersonCheckFill size={20} />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
