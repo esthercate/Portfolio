50a968c92efecd7459afa3a3af7537d1f958af0c
catherine <vuthi.esther@gmail.com>
2022-09-21 20:37:00 +0300

    adds projects component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 
 export default function Home() {
@@ -21,6 +22,7 @@ export default function Home() {
 
       <Skills />
 
+      <Projects />
       
     </div>
   )
