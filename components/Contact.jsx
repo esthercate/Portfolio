677dd300e1e9af3a247b63b9b98e5e0e50f3d1ee
catherine <vuthi.esther@gmail.com>
2022-09-23 11:33:28 +0300

    adds contact image

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="w-full lg:h-screen">
+      <div className="max-w-[1240px] mx-auto px-2 py-16 w-full">
+        <p className="text-xl tracking-widest uppercase text-[#6fb632]">
+          Contact
+        </p>
+        <h2 className="py-4">Get in Touch</h2>
+        <div className="grid lg:grid-cols-5 gap-8">
+          <div className="col-span-3 lg:col-span-2 w-full h-full p-4 shadow-xl shadow-gray-400 rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="p-4 h-full">
+              <div>
+                <Image
+                  className="rounded-xl "
+                  src="/../public/assets/contact/contact3.jpg"
+                  width="640"
+                  height="430"
+                  alt="/"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import About from '../components/About'
+import Contact from '../components/Contact'
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
 import Projects from '../components/Projects'
@@ -23,6 +24,8 @@ export default function Home() {
       <Skills />
 
       <Projects />
+
+      <Contact />
       
     </div>
   );
