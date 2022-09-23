3be15c6e19bd32ddc9913e933d3f000b68162292
catherine <vuthi.esther@gmail.com>
2022-09-23 10:39:50 +0300

    compeleted projects sections

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,43 +1,23 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import project1 from "../public/assets/projects/project1.jpg";
-import project3 from "../public/assets/projects/project3.jpg";
 
-const ProjectItem = () => {
+
+const ProjectItem = ({ title, backgroundImg,languageUsed, projectUrl }) => {
   return (
     <>
       <div className="relative flex items-center justify-center rounded-xl group hover:bg-gradient-to-r from-[#6fb632] to-[#96e6a1] h-auto w-full shadow-xl shadow-gray-400">
         <Image
           className="rounded-xl group-hover:opacity-5"
-          src={project1}
+          src={backgroundImg}
           alt="/"
         />
         <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
           <h3 className="text-2xl text-white tracking-wider text-center">
-            Hospital Finder
+            {title}
           </h3>
-          <p className="pb-4 pt-2 text-white text-center">React JS</p>
-          <Link href="/">
-            <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
-              More Info
-            </p>
-          </Link>
-        </div>
-      </div>
-
-      <div className="relative flex items-center justify-center rounded-xl group hover:bg-gradient-to-r from-[#6fb632] to-[#96e6a1] h-auto w-full shadow-xl shadow-gray-400">
-        <Image
-          className="rounded-xl group-hover:opacity-5"
-          src={project3}
-          alt="/"
-        />
-        <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-          <h3 className="text-2xl text-white tracking-wider text-center">
-            Hospital Finder
-          </h3>
-          <p className="pb-4 pt-2 text-white text-center">React JS</p>
-          <Link href="/">
+          <p className="pb-6 pt-2 text-white font-bold  text-center">{ languageUsed }</p>
+          <Link href={ projectUrl }>
             <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
               More Info
             </p>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,6 +1,7 @@
-
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import project1 from "../public/assets/projects/project1.jpg";
+import project3 from "../public/assets/projects/project3.jpg";
 
 const Projects = () => {
   return (
@@ -9,9 +10,35 @@ const Projects = () => {
         <p className="text-xl tracking-widest uppercase text-[#6fb632]">
           Projects
         </p>
-        <h2 className="py-4">What have Built</h2>
+        <h2 className="py-4">My Recent Work</h2>
         <div className="grid md:grid-cols-2 gap-8 py-6">
-         <ProjectItem />
+          <ProjectItem
+            title="Hospital Finder"
+            backgroundImg={project1}
+            languageUsed="React JS"
+            projectUrl="/project1"
+          />
+
+          <ProjectItem
+            title="Facebook Clone"
+            backgroundImg={project3}
+            languageUsed="Vue JS"
+            projectUrl="/project3"
+          />
+
+          <ProjectItem
+            title="Facebook Clone"
+            backgroundImg={project3}
+            languageUsed="Vue JS"
+            projectUrl="/project3"
+          />
+
+          <ProjectItem
+            title="Hospital Finder"
+            backgroundImg={project1}
+            languageUsed="React JS"
+            projectUrl="/project1"
+          />
         </div>
       </div>
     </div>
