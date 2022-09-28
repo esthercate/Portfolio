7a9415a349c4d744bf0741ee319b94c49184d403
catherine <vuthi.esther@gmail.com>
2022-09-28 22:10:33 +0300

    changes font family to poppins

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,16 +8,14 @@ const Hero = () => {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            Lets Build Something Together
+            Beautifully crafted web experiences.
           </p>
           <h1 className="py-4 text-gray-700">
-            Hello, I am <span className="text-[#6fb632]">Catherine</span>
+            Hi, I'm <span className="text-[#6fb632]">Catherine</span>
           </h1>
-          <h1 className="py-2 text-gray-700">Full-Stack Software Engineer</h1>
+          <h1 className="py-2 text-gray-700">Frontend Software Developer</h1>
           <p className="py-4 text-gray-400 max-w-[70%] m-auto">
-            I am a full stack web developer specializing in building exceptional
-            digital experiences. Currently, I am focused on building web
-            applications using ReactJS and Ruby on Rails tech stack.
+            I like to design and build simple and scalable frontend products with great user experiences.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
