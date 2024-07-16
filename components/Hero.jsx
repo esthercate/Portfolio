d6a3d1dee34786d30a45b0ede63a667fd4bf5449
catherine <vuthi.esther@gmail.com>
2024-07-16 23:27:50 +0300

    Changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
           <h1 className='py-4 text-gray-600 leading-[3rem]'>
             Beautifully Crafted User Experiences <br /> & Accessible Web Products!
           </h1>
-          <h3 className='pb-6 text-gray-500 max-w-[70%] m-auto'>
+          <h3 className='pb-7 text-gray-500 max-w-[70%] m-auto'>
             Hi, I'm Catherine a <span className='text-p_green'>Full-stack Software Engineer</span>.
           </h3>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
